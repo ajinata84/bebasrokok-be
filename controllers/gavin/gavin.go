64cf350483501557589e90176d2c9fcc4dd182b8
ipin131 <tarakagavin@gmail.com>
2024-06-18 15:06:05 +0700

    View jadi

--- controllers/gavin/gavin.go
+++ controllers/gavin/gavin.go
@@ -1,3 +1,20 @@
 package gavin
 
-// Get Testimonies
+import (
+	"bebasrokok-be/models"
+	"encoding/json"
+	"net/http"
+)
+
+func GetTestimonies(w http.ResponseWriter, r *http.Request) {
+	db := models.GetDB()
+
+	testimonies, err := models.GetAllTestimonies(db)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(testimonies)
+}
--- models/dbstruct.go
+++ models/dbstruct.go
@@ -70,3 +70,27 @@ func GetUserByEmail(db *sql.DB, email string) (*User, error) {
 
 	return user, nil
 }
+
+func GetAllTestimonies(db *sql.DB) ([]Testimony, error) {
+	query := "SELECT id, created_at, userid, content, aiFeedback FROM testimony"
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var testimonies []Testimony
+	for rows.Next() {
+		var testimony Testimony
+		if err := rows.Scan(&testimony.ID, &testimony.CreatedAt, &testimony.UserID, &testimony.Content, &testimony.AIFeedback); err != nil {
+			return nil, err
+		}
+		testimonies = append(testimonies, testimony)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return testimonies, nil
+}
